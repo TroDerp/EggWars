ae799ec28a34a419fbba150a7cb19226515b74d2
Marcel Kempf <marcel.kempf@tum.de>
2023-05-12 11:00:50 +0200

    make log stripping prevention backwards compatible

--- src/main/java/me/rosillogames/eggwars/listeners/PlayerInteractListener.java
+++ src/main/java/me/rosillogames/eggwars/listeners/PlayerInteractListener.java
@@ -22,10 +22,6 @@ import me.rosillogames.eggwars.utils.PlayerUtils;
 import me.rosillogames.eggwars.utils.TeamUtils;
 import me.rosillogames.eggwars.utils.VoteUtils;
 
-import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
 public class PlayerInteractListener implements Listener
 {
     @EventHandler
@@ -271,14 +267,8 @@ public class PlayerInteractListener implements Listener
     @EventHandler
     public void logStrip(PlayerInteractEvent e) {
         if (e.getAction() == Action.RIGHT_CLICK_BLOCK && e.getMaterial().toString().contains("_AXE") && e.getClickedBlock() != null) {
-            Set<Material> strippableBlocks = Stream.of(
-                    Material.ACACIA_LOG, Material.SPRUCE_WOOD, Material.OAK_LOG, Material.JUNGLE_LOG, Material.BIRCH_LOG,
-                    Material.ACACIA_WOOD, Material.BAMBOO_BLOCK, Material.BIRCH_WOOD, Material.CRIMSON_HYPHAE,
-                    Material.CRIMSON_STEM, Material.DARK_OAK_LOG, Material.DARK_OAK_WOOD, Material.JUNGLE_WOOD,
-                    Material.MANGROVE_LOG, Material.MANGROVE_WOOD, Material.OAK_WOOD, Material.SPRUCE_LOG,
-                    Material.WARPED_HYPHAE, Material.WARPED_STEM, Material.CHERRY_LOG, Material.CHERRY_WOOD
-                ).collect(Collectors.toSet());
-            if (strippableBlocks.contains(e.getClickedBlock().getType())) {
+            String b = e.getClickedBlock().getType().toString();
+            if (b.contains("_LOG") || b.contains("_WOOD") || b.contains("_STEM") || b.contains("_HYPHAE")) {
                 e.setCancelled(true);
             }
         }
